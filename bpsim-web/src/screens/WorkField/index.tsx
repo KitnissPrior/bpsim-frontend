355242dae287ce96709f9aa3f479483ab866293d
Елена Аксенова <kittrisprior@gmail.com>
2025-02-28 20:36:56 +0500

    node flow added

--- bpsim-web/src/screens/WorkField/index.tsx
+++ bpsim-web/src/screens/WorkField/index.tsx
@@ -2,12 +2,74 @@ import Toolbar from "../../shared/components/Toolbar"
 import { ItemsBar } from "./ItemsBar"
 import "./workField.css"
 import { BaseButton } from "../../shared/components/Buttons/BaseButton"
+import { createNode, getNodes } from "../../services/node.service"
+import { defaultNode } from "../../types/node"
+import { ReactFlow, Background, Controls, applyNodeChanges, applyEdgeChanges } from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+import { useCallback, useEffect, useState } from "react"
+import { Node } from "../../types/node"
+
+
 
 const WorkFieldScreen = () => {
+    const [nodesCount, setNodesCount] = useState(0);
+    const [bpsimNodes, setBpsimNodes] = useState<Node[]>([]);
+    //const flowNodes: any = [];
+
+
+    const initialNodes = [
+        {
+            id: '1',
+            position: { x: 0, y: 0 },
+            data: { label: 'Узел1' },
+        },
+        {
+            id: '2',
+            position: { x: 100, y: 100 },
+            data: { label: 'Узел2' },
+        },
+    ];
+    const initialEdges = [{ id: '1-2', source: '1', target: '2', type: "step" }];
+
+    const [nodes, setNodes] = useState(initialNodes);
+    const [edges, setEdges] = useState(initialEdges);
+
+    const onNodesChange = useCallback(
+        (changes: any) => setNodes((nds) => applyNodeChanges(changes, nds)),
+        [],
+    );
+    const onEdgesChange = useCallback(
+        (changes: any) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+        [],
+    );
+
+
+    useEffect(() => {
+        getNodes().then((response: any) => {
+            setNodesCount(response.data.length);
+            setBpsimNodes(response.data);
+            // bpsimNodes.forEach(node => {
+            //     flowNodes.push({ id: node.id, position: { x: 0, y: 0 }, data: { label: node.name } });
+            // });
+        })
+
+    }, [nodesCount])
+
+
+
+    const onNodeAddClick = () => {
+        createNode(defaultNode)
+            .then((response: any) => {
+                console.log(response.data);
+                // flowNodes.push({ id: response.data.id, position: { x: 20, y: 20 }, data: { label: response.data.name } });
+                // console.log(bpsimNodes);
+            })
+    }
+
     return (
         <div className="work-field">
             <Toolbar />
-            <ItemsBar />
+            <ItemsBar onNodeAddClick={onNodeAddClick} />
             <div className="work-field-main">
                 <div className="sidebar">
                     <BaseButton text="Создать ПО" onClick={() => console.log("Создать ПО")} />
@@ -15,11 +77,12 @@ const WorkFieldScreen = () => {
                 </div>
                 <div className="vertical-line"></div>
                 <div className="work-field-content">
-                    Моделируйте модельки и симулируйте вашу скучную жизн
+                    <ReactFlow nodes={nodes} edges={edges} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange}>
+                        <Background />
+                        <Controls />
+                    </ReactFlow>
                 </div>
-
             </div>
-
         </div>
     )
 }
--- bpsim-web/src/types/node.ts
+++ bpsim-web/src/types/node.ts
@@ -1,6 +1,14 @@
 export type Node = {
-    id: string;
-    posX: number;
-    posY: number;
-    description: string;
+    id?: string | null;
+    name?: string | null;
+    posX?: number | null;
+    posY?: number | null;
+    description?: string | null;
 }
+
+export const defaultNode: Node = {
+    name: null,
+    posX: 0,
+    posY: 0,
+    description: null
+};
