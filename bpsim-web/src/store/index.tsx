bf076496349a532080ebcef56df56209f3a05556
Елена Аксенова <kittrisprior@gmail.com>
2025-03-07 18:23:23 +0500

    node reducer added

--- bpsim-web/src/store/index.tsx
+++ bpsim-web/src/store/index.tsx
@@ -1,11 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
 import { subjectAreaReducer } from "./reducers/subjectAreaReducer";
 import { modelReducer } from "./reducers/modelReducer";
-import { configureStore } from "@reduxjs/toolkit";
+import { nodeReducer } from "./reducers/nodeReducer";
 
 export const store = configureStore({
     reducer: {
         subjectArea: subjectAreaReducer,
         model: modelReducer,
+        node: nodeReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
     devTools: true,
