ff03e75b9af50664e6fe3a34cb6f7fdae30868d3
Елена Аксенова <kittrisprior@gmail.com>
2025-04-23 20:10:47 +0500

    model items disabled when model isn't open

--- bpsim-web/src/shared/components/Tooltip/index.tsx
+++ bpsim-web/src/shared/components/Tooltip/index.tsx
@@ -4,11 +4,12 @@ import './tooltip.css';
 interface IProps {
     children: React.ReactNode;
     text: string;
+    disabled?: boolean
 }
 
-export const Tooltip: React.FC<IProps> = ({ children, text }) => {
+export const Tooltip: React.FC<IProps> = ({ children, text, disabled }) => {
     return (
-        <div className="tooltip-container">
+        <div className="tooltip-container" aria-disabled={disabled}>
             <span className="tooltip-trigger">
                 {children}
                 <span className="tooltip-text">{text}</span>
