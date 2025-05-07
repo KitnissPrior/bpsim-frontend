84b506580e4d24c94214709757ac269106534515
Елена Аксенова <kittrisprior@gmail.com>
2025-05-07 16:32:51 +0500

    workers count changed

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,12 +2,11 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-
-  workers: process.env.CI ? 1 : undefined,
+  //workers: process.env.CI ? 1 : undefined,
+  workers: 20,
   reporter: 'html',
   use: {
     baseURL: 'http://localhost:5173',
